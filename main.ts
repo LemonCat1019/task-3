bfa5a4c14a3fa88c3318e669b82c7704ca533578
LemonCat1019 <115674619+LemonCat1019@users.noreply.github.com>
2022-10-20 09:50:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+let a = 0
+input.onButtonPressed(Button.B, function () {
+    a = -1
+})
+basic.forever(function () {
+    a = 9
+    while (a >= 0) {
+        basic.showNumber(a)
+        a += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
